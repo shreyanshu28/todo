7e2769ce91c7089e286a2dd31fcb34efa70eb170
shreyanshu28 <shreyanshu28@github.com>
2020-08-13 00:49:04 +0530

    Added dark theme

--- scripts/script.js
+++ scripts/script.js
@@ -64,3 +64,11 @@ function ButtClick() {
 }
 
 ButtClick();
+
+document
+  .getElementById("themeSwitch")
+  .addEventListener("change", function (Event) {
+    event.target.checked
+      ? document.body.setAttribute("data-theme", "dark")
+      : document.body.removeAttribute("data-theme");
+  });
